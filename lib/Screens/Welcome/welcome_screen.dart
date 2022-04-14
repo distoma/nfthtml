f17c300681106903d16b48a66da4825847c85d76
distoma <distoma@gmial.com>
2022-04-14 12:04:42 +0900

    method changing

--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -4,6 +4,8 @@ import 'package:nftminthtml/Screens/Welcome/components/body.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () {});
+
     return Scaffold(
       body: Body(),
     );
